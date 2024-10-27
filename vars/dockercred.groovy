fdb3d7f4a478bf171daaf80d1e60a43403d537c5
Chandu-valasa <168898171+Chandu-valasa@users.noreply.github.com>
2024-10-27 15:39:10 +0530

    Update dockercred.groovy

--- vars/dockercred.groovy
+++ vars/dockercred.groovy
@@ -1,9 +1,9 @@
 /* groovylint-disable LineLength */
 /* groovylint-disable-next-line NglParseError */
-def call(Docker, dockerpass, dockeruser){
+def call(String dockeruser, String image,String tag){
     withCredentials([usernamePassword(credentialsId:"Docker",passwordVariable:"dockerpass",usernameVariable:"dockeruser")]) {
                 sh 'docker login -u ${dockeruser} -p ${dockerpass}'
-                sh 'docker tag shekar:latest ${dockeruser}/shekar:latest'
-                sh 'docker push ${dockeruser}/shekar:latest'
-               }
+    }
+                sh 'docker push ${dockeruser}/${image}:${tag}'
+               
 }
