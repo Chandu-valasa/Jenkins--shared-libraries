f164cbf3338137afbc11e65d7c82e61c7dd06899
Chandu-valasa <168898171+Chandu-valasa@users.noreply.github.com>
2024-10-27 16:53:05 +0530

    Update dockercred.groovy

--- vars/dockercred.groovy
+++ vars/dockercred.groovy
@@ -4,6 +4,6 @@ def call(String dockeruser, String image,String tag){
     withCredentials([usernamePassword(credentialsId:"Docker",passwordVariable:"dockerpass",usernameVariable:"dockeruser")]) {
                 sh 'docker login -u ${dockeruser} -p ${dockerpass}'
     }
-                sh 'docker push ${dockeruser}/${image}:${tag}'
+                sh "docker push ${dockeruser}/${image}:${tag}"
                
 }
